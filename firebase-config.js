1e998f8c859efccaca0954e987db523d20e8fbaa
JJO474 <k2296082@naver.com>
2026-08-28 10:57:45 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,11 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCrYDFenwsBfrrEe8-eCN1PmL5HHYyTpqg",
   authDomain: "jjo4-94ae9.firebaseapp.com",
+  databaseURL: "https://jjo4-94ae9-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "jjo4-94ae9",
   storageBucket: "jjo4-94ae9.firebasestorage.app",
   messagingSenderId: "145730180775",
   appId: "1:145730180775:web:653692738d0b2325b6ad82"
 };
+
+export const syncRoom = "baeiple-story-main";
